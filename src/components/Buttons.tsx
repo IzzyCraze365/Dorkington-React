1a61a0d4cc259f6563916213f106e4ab1bcc37b3
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-06-16 01:05:31 -0400

    Bootstrap dependency added

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+  children: string;
+  color?: string; // Otional
+  onClick: () => void;
+}
+
+const Buttons = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      Buttons: {children}
+    </button>
+  );
+};
+
+export default Buttons;
