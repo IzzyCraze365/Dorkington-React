637a611bc7b6292d36a46de65c2db9e02b6a2e50
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-06-25 19:31:01 -0400

    Backlog component worked on

--- src/components/HUD/Backlog.tsx
+++ src/components/HUD/Backlog.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
 
 // Base this off of the ListGroup.tsx but make it so every thing is saved in an array
-const Backlog = () => {
-  return (
-    <div>Backlog</div>
-  )
+
+interface backlogHistoryProps {
+  actions: string[];
+  onSelection: (action: string) => void; //similar to an onClick action
 }
 
-export default Backlog
+function backlogHistory({ actions, onSelection }: backlogHistoryProps) {
+  // Initially no item is selected.
+  const [actionIndex, setActionIndex] = useState(-1);
+}
+
+const Backlog = () => {
+  return (
+    <>
+      <div>Backlog</div>
+      {actions.length === 0 && <p>Not Started</p>}
+      <ul className="backlog">
+        {actions.map((action, index) => (
+          <li
+            key={action}
+            onClick={() => {
+              setActionIndex(index);
+              onSelection(action);
+            }}
+            className={
+              actionIndex === index ? "backlog-action active" : "backlog-action"
+            }
+          >
+            {action}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Backlog;
