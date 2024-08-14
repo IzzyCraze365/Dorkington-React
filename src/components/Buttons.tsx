bcf90f07c831f027067fac92062ea92eb947bf7f
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-08-14 12:47:42 -0400

    Buttons notes

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface Props {
   children: string;
-  color?: string; // Optional Color here to change
+  color?: string; // Optional Color here to changed
   
   onClick: () => void;
 }
